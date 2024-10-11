fd04cebd913a10b859e982a9ccca0e217b2fdc94
SsnL <tongzhou.wang.1994@gmail.com>
2024-10-11 16:55:07 -0700

    cap

--- airbench94_spectral_safe.py
+++ airbench94_spectral_safe.py
@@ -511,7 +511,7 @@ def eval_autoattack(model, loader):
     model.eval()
     adversary = AutoAttack(lambda x: model(loader.normalize(x)), norm='Linf', eps=8/255, version='rand')
     x, y = loader.images, loader.labels
-    x = x * CIFAR_STD[:, None, None] + CIFAR_MEAN[:, None, None]
+    x = x * CIFAR_STD[:, None, None].cuda() + CIFAR_MEAN[:, None, None].cuda()
     assert x.max() <= 1 and x.min() >= 0
     x_adv = loader.normalize(adversary.run_standard_evaluation(x, y, bs=1024))
     return (infer_basic(x_adv, model).argmax(1) == y).float().mean().item()

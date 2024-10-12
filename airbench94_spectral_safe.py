c7611767031f9202638fa1f8d4fc4ca07091da7f
SsnL <tongzhou.wang.1994@gmail.com>
2024-10-11 19:52:45 -0700

    cap

--- airbench94_spectral_safe.py
+++ airbench94_spectral_safe.py
@@ -519,12 +519,12 @@ class GradContiguous(torch.autograd.Function):
 def eval_autoattack(model, loader):
     model.eval()
     adversary = AutoAttack(
-        lambda x: model(loader.normalize(GradContiguous.apply(x)).half().to(memory_format=torch.channels_last)).float(),
+        lambda x: model(loader.normalize(x).half().to(memory_format=torch.channels_last)).float(),
         norm='Linf', eps=8/255, version='rand')
     x, y = loader.images, loader.labels
     x = x.float() * CIFAR_STD[:, None, None].to(x.device, dtype=x.dtype) + CIFAR_MEAN[:, None, None].to(x.device, dtype=x.dtype)
     assert x.max() <= 1 and x.min() >= 0
-    x_adv = loader.normalize(adversary.run_standard_evaluation(x, y, bs=1024)).half()
+    x_adv = loader.normalize(adversary.run_standard_evaluation(x.to(memory_format=torch.contiguous_format), y, bs=1024)).half()
     return (infer_basic(x_adv, model).argmax(1) == y).float().mean().item()
 
 def evaluate(model, loader, tta_level=0):

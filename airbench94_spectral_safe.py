cdea05ab84898c8363461bd130ff7fe33df490af
SsnL <tongzhou.wang.1994@gmail.com>
2024-10-12 00:54:52 -0700

    test2

--- airbench94_spectral_safe.py
+++ airbench94_spectral_safe.py
@@ -362,9 +362,9 @@ class SafeInputNet(nn.Module):
     def forward(self, x):
         # qx: [B, imC, H, W]
         perturb_distribution = torch.distributions.uniform.Uniform(-self.perturb_half_range, self.perturb_half_range)
-        # qx = x + perturb_distribution.sample(x.shape[:-3])
+        qx = x + perturb_distribution.sample(x.shape[:-3])
         # qx = torch.lerp(x, torch.randn_like(x), 0.65)
-        qx = self.blur(x + perturb_distribution.sample(x.shape[:-3])) + perturb_distribution.sample(x.shape[:-3])
+        # qx = self.blur(x + perturb_distribution.sample(x.shape[:-3])) + perturb_distribution.sample(x.shape[:-3])
         # x: [B, imC, H, W]
         l_mult, base_l = self.safe_part(qx)  # [B, nClass, imC, H, W]
         if isinf(self.inp_grad_norm_p):
